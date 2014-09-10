6941ec700968e8d79114675f35e5c1406c8b2bf1
Nathan Pabrai <nathanpabrai@gmail.com>
2014-09-09 20:07:32 -0400

    Initial Commit

---
+++ BitPulse/BPViewController.h
@@ -0,0 +1,13 @@
+//
+//  BPViewController.h
+//  BitPulse
+//
+//  Created by Nathan Pabrai on 9/9/14.
+//  Copyright (c) 2014 Nathan Pabrai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPViewController : UIViewController
+
+@end
---
+++ BitPulse/BPViewController.m
@@ -0,0 +1,29 @@
+//
+//  BPViewController.m
+//  BitPulse
+//
+//  Created by Nathan Pabrai on 9/9/14.
+//  Copyright (c) 2014 Nathan Pabrai. All rights reserved.
+//
+
+#import "BPViewController.h"
+
+@interface BPViewController ()
+
+@end
+
+@implementation BPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
